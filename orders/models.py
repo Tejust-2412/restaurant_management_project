b19c5b264bbbb9477623e9c6c59e704ae4aeb098
Tejas Kadam <kadamtejust2412@gmail.com>
2025-09-19 15:42:49 +0530

    49402

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Coupon(models.Model) :
+    code = models.CharFied(max_lenth = 20 , unique = True)
+    discount = models.DescimalField(max_lenth = 5 , decimal_places = 2 )
+    discount = models.DescimalField(max_digith = 5  , decimal_places= 2)
+    created_at = models.DateTimeField (auto_now_add = True)
+
+
+    def __str__(self):
+        reeturn self.code
