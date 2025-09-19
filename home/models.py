855ff772c1c5904b5f214129c4c0ac8fa5f73df4
Tejas Kadam <kadamtejust2412@gmail.com>
2025-09-19 15:14:38 +0530

    49403

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class MenuCategory (models,Model):
+    name = models.CharFied(max_length = 100 , unique = True )
+
+
+    class Meta :
+        verbose_name_plural = "Menu Categories"
+
+    def __str__(self):
+        return self.name 
