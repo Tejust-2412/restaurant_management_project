3d9c2bfd54ec23824f43e66c241205efac6572c3
Tejas Kadam <kadamtejust2412@gmail.com>
2025-09-21 19:58:02 +0530

    49400

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Coupon
+from .models import Coupon , Order , OrderStatus
 # Register your models here.
 
 @admi.register(Coupon)
@@ -7,3 +7,6 @@ class CouponAdmin(admin.ModelAdmin):
     list_display = ('id' , 'code' , 'discount' , 'created')
     search_fiedls = ('code' ,)
     list_filter = ('created_at' ,)
+
+admin.site.register(OrderStatus)
+admin.site.register(Order)
