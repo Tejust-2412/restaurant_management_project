1e7e5cdd373120c21b24e32ba12f781f4ad6e7f6
Tejas Kadam <kadamtejust2412@gmail.com>
2025-09-21 19:46:46 +0530

    49401

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def create_default_statuses(sender , **kwargs) :
+    from .models import OrderStatus
+    from .import  ORDER_STATUSES
+    for status in ORDER_STATUSES :
+        OrderStatus.objects.get_or_create(name=status)
+
+
+    
 
 
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+
