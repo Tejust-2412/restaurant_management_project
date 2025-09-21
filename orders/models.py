d48c3b3a74a790f519b55e802847273afd3bee9d
Tejas Kadam <kadamtejust2412@gmail.com>
2025-09-21 19:56:18 +0530

    49400

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from .models import OrderStatus 
 # Create your models here.
+class Order(models.Model):
+    customer = models.ForeighnKey(User , on_dekete = models.CASCADE)
+    product = models.CharField(max_length = 100)
+    quantity = models.PositiveIntegerField(default = 1)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    status = models.ForeighKey(
+        OrderStatus , 
+        on_delete = models.SET_NULL , 
+        null = True , 
+        blank = True ,
+        related_name = "orders"
+    )
+
+    def __str__(self) :
+        return f"Order #{self.id} - {self.product} ({self.status})"
+
+
 class OrderStatus(models..Model):
     name = models.CharField(max_length = 50 , unique = True)
 
