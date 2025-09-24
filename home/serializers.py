4d6e4687d917aca15f417446c1d8eb50ae01c00b
Tejas Kadam <kadamtejust2412@gmail.com>
2025-09-24 10:43:05 +0530

    50330

--- home/serializers.py
+++ home/serializers.py
@@ -5,4 +5,9 @@ from .model import MenuCategory
 class MenuCategorySerilizer(serializers.ModelSerializer) :
     clas Meta :
     model = MenuCategory
-    fields = ['id' , 'name']
+    fields = ['id' , 'name' , 'description', 'price' , 'available' , 'category']
+
+    def validate_price(self , value) :
+        if value <= 0 :
+            raise serializers.ValidationError('Price must be greater than zero')
+        return value
